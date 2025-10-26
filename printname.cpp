c7ea4f7cbcda96a5c86fd461f46c97577253054d
unzilanayab <unzilanayab06@gmail.com>
2025-10-26 13:53:17 +0500

    print name sujbect

---
+++ printname.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Name: Unzila Nayab" << endl;
+    cout << "Registration Number: 23222" << endl;
+    cout << "Course: Software Construction and Development" << endl;
+    return 0;
+}
+
